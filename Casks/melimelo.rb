b695ddbe60ef452b3115472661d9284539a3e676
Guillaume Buisson <guillaume@gbuisson.com>
2026-03-11 17:46:10 +0100

    melimelo: use self-hosted download URL
    Private GitHub releases can't be downloaded by brew (no auth).
    Host zips on gbuisson.com/downloads/melimelo/ instead.

--- Casks/melimelo.rb
+++ Casks/melimelo.rb
@@ -2,8 +2,7 @@ cask "melimelo" do
   version "0.5-beta"
   sha256 "2089efe17dcabae1102b55048b20af8c913e2ff42827634e8687a69a7e69c633"
 
-  url "https://github.com/gbuisson/MeliMelo/releases/download/v#{version}/MeliMelo-v#{version}-macos.zip",
-      verified: "github.com/gbuisson/MeliMelo/"
+  url "https://gbuisson.com/downloads/melimelo/MeliMelo-v#{version}-macos.zip"
 
   name "MeliMelo"
   desc "Remote desktop streaming for macOS"
